f5f8477dfd912ad69cd3946ecb71f5adcf68295c
Sam <contactsamie@gmail.com>
2013-10-03 23:13:02 -0400

    Sam Uploaded files

---
+++ unbox/unbox/dev/unbox.js
@@ -0,0 +1,264 @@
+﻿(function (w) {
+    var _dependency = function () { };
+    var pDepend = _dependency.prototype;
+    var logger = function (o) {
+       // console.log(o);
+    };
+
+    var errorLog = function (o) {
+        console.error(o);
+    };
+    var warnLog = function (o) {
+        console.warn(o);
+    };
+    var errorCodes = {
+        falsyInput: "Invalid input parameter",
+        wrongDependency: "Suplied dependency does not exist"
+    };
+    var utility = {
+        basicInputValidation: function (i, types) {
+            i || errorLog(errorCodes.falsyInput);
+        },
+        trim: function (s) {
+            return s.replace(/^\s+|\s+$/g, '');
+        }
+    };
+    var modules = {};
+    var BASE = {};
+    modules.names = {};
+    modules.dependencies = {};
+    var dependencyFactory = function (d) {
+        var dependsOn = [];
+        for (var i = 0; i < d.length; i++) {
+            var _dep = modules.dependencies[d[i]];
+
+            _dep ? dependsOn.push(_dep) : warnLog(errorCodes.wrongDependency);
+        }
+        return dependsOn;
+    };
+    var thisDependencyInjection = {
+    };
+
+    var pullApartMethod = function (m) {
+        var mA = m.match(/^function\s*[^\(]*\(\s*([^\)]*)\)/m);
+        var arg = mA[1].split(',');
+        return {
+            arg: arg
+        }
+    };
+    var iiException = function (m) {
+        this.message = m;
+    };
+    var dependencyExtractor = function (str) {
+        var arg = (pullApartMethod(str)).arg;
+        for (var i = 0; i < arg.length; i++) {
+            arg[i] = utility.trim(arg[i]);
+        }
+
+        return arg;
+    }
+
+    var dependencyMethodFactory = function (obj, f, dependency) {
+        logger("building dependencies ....");
+
+        logger(f);
+
+        var methdBuilder = "function(" + dependency.toString() + "){\n\n " + w.iinject.module.mgmt.getDeclareList(obj.me).code + "\n\n  return (" + f.toString() + ")(); \n\n  }";
+        logger("executing ...");
+        logger(methdBuilder);
+        eval("var fInject=" + methdBuilder);
+        logger(fInject);
+        return fInject;
+    };
+    var controllerFactory = function (obj, methodRef) {
+        var execute = function (f) {
+            var executionResult = undefined;
+            var iDependsOn = methodRef && (w.iinject.module.mgmt.getDependency(obj.me, methodRef))["dependencyList"];//  w.iinject.module.all[obj.me]["dependencies"];
+
+            utility.basicInputValidation(f);
+            var dependsOn = {};
+            var dependency = [];
+            if (iDependsOn) {
+                logger("injecting alternate api: arg:");
+                dependency = iDependsOn;
+            } else {
+                dependency = dependencyExtractor(f.toString());
+                logger("injecting default api: arg:");
+            }
+            logger(dependency);
+            dependsOn = dependencyFactory(dependency);
+            logger(dependsOn);
+            //logger(f);
+            try {
+                var fInject = function () { };
+                if (iDependsOn) {
+                    fInject = dependencyMethodFactory(obj, f, dependency);
+                }
+                logger("with..");
+                logger(dependsOn);
+                var root = w.iinject.module.mgmt.getRoot(obj.me);
+                logger("at root space ...");
+                logger(root);
+                root.current(fInject);
+                executionResult = root.current.space.apply(null, dependsOn);
+                logger(root.current);
+                logger("method executes well ......................................................");
+            } catch (exc) {
+                errorLog("exception occured while executing mehod");
+                errorLog(exc);
+            }
+
+            return executionResult;
+        };
+
+        obj = obj || {};
+
+        obj.run = execute;
+
+        return obj;
+    }
+    var injectionFactory = function (name, that, methodDependency) {
+        return function () {
+            var rArguments = [];
+            for (var i = 0; i < arguments.length; i++) {
+                rArguments.push(arguments[i].toString());
+            }
+            logger(rArguments);
+            // w.iinject.module.all[name]["dependencies"] = rArguments;
+
+            methodRef = w.iinject.module.mgmt.setDependency(name, rArguments);
+
+            return controllerFactory(that, methodRef);
+        };
+    };
+    var ModuleFactory = function (name) {
+        modules.names[name] = new _dependency();
+        var d = modules.names[name];
+        d = controllerFactory(d);
+        d.me = name;
+        d.declare = function (item) {
+            w.iinject.module.mgmt.pushDeclare(name, item);
+            this.declare.variables = item;
+            return this;
+        }
+        d.inject = injectionFactory(name, d);
+
+        return d;
+    };
+
+    pDepend.getDependencies = function () {
+        return modules.dependencies;
+    };
+    pDepend.me = "";
+    pDepend.method = function (name, f) {
+        utility.basicInputValidation(typeof f === "function");
+        utility.basicInputValidation(name);
+        name = utility.trim(name);
+        modules.dependencies[name] = f;
+        logger("injected .." + name);
+        logger(modules.dependencies);
+        return this;
+    };
+    w.iinject = function (name) {
+        if (typeof name === "string") {
+            logger(name + " module finding");
+            if (w.iinject.module.all[name]) {
+                logger(name + " module found");
+                return w.iinject.module.all[name]["build"];
+            } else {
+                logger(name + " module does not exist");
+                logger("creating " + name + " module ......");
+                return w.iinject.module(name);
+            }
+        } else {
+            throw new iiException("module reference error");
+        }
+    };
+    w.iinject.module = function (name) {
+        w.iinject.module.all = w.iinject.module.all || {};
+
+        if (typeof name === "string") {
+            name = utility.trim(name);
+            if (w.iinject.module.all[name]) {
+                throw new iiException(name + " module already exist");
+            } else {
+                utility.basicInputValidation(name);
+                name = utility.trim(name);
+                var build = ModuleFactory(name);
+
+                w.iinject.module.all[name] = {
+                    build: build, dependencies: {}, root: {
+                        space: {
+                            current: function (f) {
+                                logger("current method running:%%%%%%%%%%%%%%%%%");
+                                logger(f);
+                                this.current.space = f;
+                            }
+                        }, declare: {}
+                    }
+                };
+                logger(w.iinject.module.all[name]["build"]);
+                return build;
+            }
+        }
+    };
+    w.iinject.module.all = {};
+    w.iinject.module.mgmt = {
+        methodCounter: function () {
+            w.iinject.module.mgmt.methodCounter.i = w.iinject.module.mgmt.methodCounter.i || 0;
+            w.iinject.module.mgmt.methodCounter.i++;
+            return "methodId" + w.iinject.module.mgmt.methodCounter.i;
+        },
+        getDependency: function (moduleName, methodRef) {
+            w.iinject.module.all[moduleName]["dependencies"] = w.iinject.module.all[moduleName]["dependencies"] || {};
+            return w.iinject.module.all[moduleName]["dependencies"][methodRef];
+        },
+        setDependency: function (moduleName, methodNames) {
+            var result = false;
+            if (w.iinject.module.all[moduleName] && w.iinject.module.all[moduleName]["dependencies"]) {
+                var mathodRef = this.methodCounter();
+                w.iinject.module.all[moduleName]["dependencies"][mathodRef] = {
+                    refName: mathodRef,
+                    dependencyList: methodNames,
+                    dependencyStore: {}
+                };
+
+                for (var i = 0; i < methodNames.length; i++) {
+                    var methodName = methodNames[i];
+                    if (methodName) {
+                        w.iinject.module.all[moduleName]["dependencies"][mathodRef]["dependencyStore"][methodName] = function () { };
+                    }
+                }
+
+                result = mathodRef
+            }
+            return result;
+        },
+        pushDeclare: function (moduleName, rootItem) {
+            if (rootItem) {
+                w.iinject.module.all[moduleName]["root"]["declare"] = rootItem;
+            }
+            return w.iinject.module.all[moduleName]["root"]["declare"];
+        },
+        getDeclareList: function (moduleName) {
+            var obj = w.iinject.module.all[moduleName]["root"]["declare"];
+            var declStr = {
+                code: "",
+                parameters: "window"
+            };
+
+            for (var item in obj) {
+                if (obj.hasOwnProperty(item)) {
+                    declStr.code += "\nvar " + item + "=" + "window.iinject('" + moduleName + "').declare.variables." + item + ";";
+                    declStr.parameters += "," + item + "\n\n";
+                }
+            }
+            return declStr;
+        },
+        getRoot: function (moduleName) {
+            return w.iinject.module.all[moduleName]["root"]["space"];
+        },
+        getBuild: function (moduleName) { },
+        setBuild: function (moduleName, build) { }
+    };
+})(window);
---
+++ unbox/unbox/tests/unbox.test.js
@@ -0,0 +1,209 @@
+﻿test("iinject library API consistency test - direct instanciation methods", function () {
+    ok(iinject, "iinject object must be available in global space");
+    ok(typeof iinject === "function", "iinject object is a function");
+    ok(iinject.module, "iinject.module must be accessible");
+    ok(typeof iinject.module === "function", "iinject.module must be a function");
+    ok(iinject("a1"), "an instance of iinject is possible via iinject()");
+    ok(iinject("myName").me === "myName", "the module name must be retrievable from 'me' object off of  iinject()");
+
+    ok(typeof iinject("a2").method === "function", "an instance of iinject() has a function 'method' off of it");
+
+    ok(typeof iinject("a31").inject === "function", "an instance of iinject() has a function 'inject' off of it");
+
+    ok(typeof iinject("a32").run === "function", "an instance of iinject() has a function 'run' off of it");
+
+    ok(typeof iinject("a4").method("b1").inject === "function", "an instance of iinject() has a function 'method().inject' off of it");
+
+    ok(typeof iinject("a5").method("b2").run === "function", "an instance of iinject() has a function 'method().run' off of it");
+
+    ok(typeof iinject("a6").inject("b3").run === "function", "an instance of iinject() has a function 'inject().run' off of it");
+    ok(typeof iinject("a7").inject("b4").method === "function", "an instance of iinject() has a function 'inject().method' off of it");
+
+    ok(typeof iinject("a8").run(function () { }) === "undefined", "an instance of iinject().run has NO object  off of it");
+
+    ok(typeof iinject("a33").declare === "function", "an instance of iinject() has a function 'attatch' off of it");
+    ok(typeof iinject("a34").declare(function () { }).run === "function", "an instance of iinject().attatch has a function 'run' off of it");
+    ok(typeof iinject("a35").declare(function () { }).method === "function", "an instance of iinject().attatch has a function 'method' off of it");
+    ok(typeof iinject("a36").declare(function () { }).inject === "function", "an instance of iinject().attatch has a function 'inject' off of it");
+});
+
+test("It is possibe to make run() pass object set in declare() to method," +
+    " and the method should run as specified by that method - With compound  instanciation patterns of iinject", function () {
+        var testMethodToInject = function (parameter) { return typeof parameter === "function" ? parameter(100) : (parameter + 100); };
+        var expected = function (current) { return testMethodToInject(current); };
+        expected.declarations = [
+            "-3", "-2", "-1", " 0", 00, 0.0, 0.1, 12.34, 0.0005,
+            "a", "b", "c", "d", "e", "f", "h",
+            function (a) { return a * 3; }, function (a) { return a + "sam"; }, function () { },
+            ["1"], {}, []
+        ];
+        var methods = {};
+        for (var j = -100; j < 100; j++) {
+            expected.declarations.push(j);
+            expected.declarations.push({ number: j });
+            expected.declarations.push([j]);
+
+            eval(" methods['fun_" + j + "']=function(a){return a+" + j + ";}");
+            expected.declarations.push(methods["fun_" + j]);
+        }
+
+        var total = expected.declarations.length;
+
+        for (var i = 0; i < total; i++) {
+            var expecting = expected(expected.declarations[i]);
+
+            var result1 =
+                iinject("module1").
+                declare({ variable: expected.declarations[i] }).
+                method("method", testMethodToInject).
+                inject("method").
+                run(function () { return method(variable); });
+            ok(result1 === expecting,
+               "MODULE " + iinject("method").me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result1 +
+                " when  chain iinject().declare().method().inject().run() , declare()'s param is accessible in run() which inject() injects with method()'s param");
+
+            var module2 = iinject("module2");
+            var result2 =
+               module2.
+               declare({ variable: expected.declarations[i] }).
+               method("method", testMethodToInject).
+               inject("method").
+               run(function () { return method(variable); });
+            ok(result2 === expecting,
+              "MODULE " + module2.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result2 +
+                " when   chain  var module = iinject('module'); module.declare().method().inject().run() , declare()'s param is accessible in " +
+                " run() which inject() injects with method()'s param");
+
+            var module3 = iinject("module3").declare({ variable: expected.declarations[i] });
+            var result3 =
+               module3.
+               method("method", testMethodToInject).
+               inject("method").
+               run(function () { return method(variable); });
+            ok(result3 === expecting,
+              "MODULE " + module3.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result3 +
+                " when   chain  var module = iinject('module').declare(); module.method().inject().run() , declare()'s param is accessible in " +
+                "  run() which inject() injects with method()'s param");
+
+            var module4 = iinject("module4").declare({ variable: expected.declarations[i] }).method("method", testMethodToInject);
+            var result4 =
+               module4.
+               inject("method").
+               run(function () { return method(variable); });
+            ok(result4 === expecting,
+               "MODULE " + module4.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result4 +
+                " when   chain  var module = iinject('module').declare().method(); module.inject().run() , declare()'s param is accessible in " +
+                "  run() which inject() injects with method()'s param");
+
+            var module5 = iinject("module5").declare({ variable: expected.declarations[i] }).method("method", testMethodToInject).inject("method");
+            var result5 =
+               module5.
+               run(function () { return method(variable); });
+            ok(result5 === expecting,
+              "MODULE " + module5.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result5 +
+                " when   chain  var module = iinject('module').declare().method().inject(); module.run() , declare()'s param is accessible in " +
+                "  run() which inject() injects with method()'s param");
+
+            var module6 = iinject("module6");
+            module6.declare({ variable: expected.declarations[i] });
+            module6.method("method", testMethodToInject);
+            module6.inject("method");
+            var result6 = module6.run(function () { return method(variable); });
+            ok(result6 === expecting,
+                "MODULE " + module6.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result6 +
+                " when pattern of instantiation is  var module = iinject('module');/ module.declare();/ module.method();/ module.inject();/ module.run() , declare()'s param is accessible in " +
+                "  run() which inject() injects with method()'s param");
+
+            var module7 = iinject("module7");
+            module7.declare({ variable: expected.declarations[i] });
+            module7.inject("method");
+            module7.method("method", testMethodToInject);
+
+            var result7 = module7.run(function () { return method(variable); });
+            ok(result7 === expecting,
+                "MODULE " + module7.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result7 +
+                "  YOU CAN INJECT BEFORE SPECIFYING THE METHOD TO BE INJECTED");
+
+            iinject("module77").declare({ variable: expected.declarations[i] });
+            iinject("module77").inject("method");
+            iinject("module77").method("method", testMethodToInject);
+
+            var result77 = iinject("module77").run(function () { return method(variable); });
+            ok(result77 === expecting,
+                "USING iinject('module') as instance,  MODULE " + iinject("module77").me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result77 +
+                "  YOU CAN INJECT BEFORE SPECIFYING THE METHOD TO BE INJECTED");
+
+            var module8 = iinject("module8");
+
+            module8.inject("method");
+            module8.method("method", testMethodToInject);
+            module8.declare({ variable: expected.declarations[i] });
+            var result8 = module8.run(function () { return method(variable); });
+            ok(result8 === expecting,
+                "MODULE " + module8.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result8 +
+                "  YOU CAN DECLARE VARIABLES JUST BEFORE RUN AND AFTER EVERY THING ELSE");
+
+            iinject("module88").inject("method");
+            iinject("module88").method("method", testMethodToInject);
+            iinject("module88").declare({ variable: expected.declarations[i] });
+            var result88 = iinject("module88").run(function () { return method(variable); });
+            ok(result88 === expecting,
+               "USING iinject('module') as instance,  MODULE " + iinject("module88").me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result88 +
+                "  YOU CAN DECLARE VARIABLES JUST BEFORE RUN AND AFTER EVERY THING ELSE");
+
+            var module9 = iinject("module9");
+            module9.inject("method");
+            module9.declare({ variable: expected.declarations[i] });
+            module9.method("method", testMethodToInject);
+            var result9 = module9.run(function () { return method(variable); });
+            ok(result9 === expecting,
+                "MODULE " + module9.me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result9 +
+                "  YOU CAN DECLARE VARIABLES AFTER INJECT AND BEFORE METHOD AND AFTER EVERY THING ELSE");
+
+            iinject("module99").inject("method");
+            iinject("module99").declare({ variable: expected.declarations[i] });
+            iinject("module99").method("method", testMethodToInject);
+            var result99 = iinject("module99").run(function () { return method(variable); });
+            ok(result99 === expecting,
+               "USING iinject('module') as instance,  MODULE " + iinject("module99").me + " - with 'declaration({v:" + expected.declarations[i] + "})'  " + "expecting " + expecting + " but got " + result99 +
+                "  YOU CAN DECLARE VARIABLES AFTER INJECT AND BEFORE METHOD AND AFTER EVERY THING ELSE");
+        }
+    });
+
+test("iinject dependency injection test - Accessing 'declare' parameters from 'run'", function () {
+    var testModule = iinject("myModule1");
+    testModule.declare({
+        init: 10,
+        parameter: 100
+    });
+    testModule.method("testMethod1", function (arg1, arg2) {
+        return arg1 + arg2;
+    });
+    testModule.inject("testMethod1");
+
+    var result = testModule.run(function () {
+        var result = testMethod1(parameter, init);
+        return result;
+    });
+    ok(result === 110, "When properties are declared using the declare method, those properties must be available in 'method' and 'run' ");
+
+});
+
+test("iinject dependency injection test - Accessing 'declare' parameters from 'method'", function () {
+    var testModule = iinject("myModule2");
+    testModule.declare({
+        init: 10,
+        parameter: 100
+    });
+    testModule.method("testMethod2", function () {
+        return init;
+    });
+    testModule.inject("testMethod2");
+
+    var result = testModule.run(function () {
+        var result = testMethod2();
+        return result;
+    });
+    ok(result === undefined, "When properties are declared using the declare method, those properties must NOT be available in 'method' and 'method' ");
+
+});
